ce8b5f5a1fc1c7c978ade406e40d9f042e11a6e1
KaioDev08 <kaioaraujo6190@outlook.com>
2022-07-25 06:37:51 -0300

    Primeiro commit

---
+++ js/produtos.js
@@ -0,0 +1,62 @@
+const arrayProdutos = [
+        {
+            "id":1,
+            "nome": "Xiami Redmi Note 8",
+            "preco": "760,00",
+            "image": "assets/xiami redmi note 8.jpeg"
+        },
+        {
+            "id":2,
+            "nome": "Relógio smartwatch",
+            "preco": "496,00",
+            "image": "assets/Relógio smartwatch.jpg"
+        },
+        {
+            "id":3,
+            "nome": "Smartphone Samsung Galaxy A23 Branco",
+            "preco": "1579,00",
+            "image": "assets/Smartphone Samsung Galaxy A23 Branco.jpg",
+        },
+        {
+            "id":4,
+            "nome": "Computador Pichau Gamer",
+            "preco": "2499,99",
+            "image": "assets/computador pichau gamer.jpg"
+        },
+        {
+            "id":5,
+            "nome": "Headset Gamer RedDragon",
+            "preco": "299,99",
+            "image": "assets/headset gamer reddragon.jpg"
+        },
+        {
+            "id":6,
+            "nome": "Monitor Dell 21.5",
+            "preco": "899,99",
+            "image": "assets/Monitor 21.5 Dell.jpg"
+        },
+        {
+            "id":7,
+            "nome": "Mouse Pad Hyperx",
+            "preco": "85,00",
+            "image": "assets/Mouse pad hyperx.jpg"
+        },
+        {
+            "id":8,
+            "nome": "Mouse RedDragon",
+            "preco": "150,00",
+            "image": "assets/mouse reddragon.jpg"
+        },
+        {
+            "id":9,
+            "nome": "Pc Gamer Itx",
+            "preco": "3500,00",
+            "image": "assets/pc gamer itx.jpg"
+        },
+        {
+            "id":10,
+            "nome": "Teclado gamer hyperx",
+            "preco": "250,00",
+            "image": "assets/teclado gamer hyperx.jpg"
+        },
+    ];
