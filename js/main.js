8f9fcfa3f4fb18afb41a26f56ad5c22129876f66
KaioDev08 <Kaioaraujo6190@outlook.com>
2022-07-26 16:11:06 -0300

    Função de adicão de item na bag

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 
-
-
 const campoList = document.querySelector('#list');
 
 //Fazendo a listagem de produtos dos items buscados
@@ -14,33 +12,83 @@ function listarSearch(array) {
                         <h2 class="titleProduct">${element.nome}</h2>
                         <p class="description">Valor : R$${element.preco}</p>
                     </div> 
-                </button>
+            </button>
             </li>
          `;
+
+
     })
 }
 
+
 listarSearch(arrayProdutos)
+adicionandoEventButton();
 
-const inputSearch = document.querySelector('#inputSearch');
+//Adicionando evento de clique em cada produto 
+function adicionandoEventButton() {
+    const buttons = document.querySelectorAll('#buttonList');
+    buttons.forEach((element) => {
+        element.addEventListener('click', () => {
+            addItensBag(element.parentNode.id);
+        });
+    })
 
-inputSearch.addEventListener('keyup', (event) =>{keyUpping(event)})
+}
 
-function keyUpping(event){
-    const valorDigitado = event.target.value;
-
-    const arraySearch = searchItems(valorDigitado);
-    
-    console.log(arraySearch);
-
-    campoList.innerHTML = '';
-    listarSearch(arraySearch)
+//Adicionando items a bag
+const itemsBag = [];
+function addItensBag(id) {
+    itemsBag.push(arrayProdutos[id]);
+    exibirBag(itemsBag);
     
 }
 
+ const FieldList = document.querySelector('#listBag');
+ function exibirBag(itemsBag) {
+    console.log(itemsBag)
+    FieldList.innerHTML = '';
+    itemsBag.forEach(element =>{
+        FieldList.innerHTML += `
+        <li class="itemList itemListBag" id="${element.id}">
+            <img class="img" src="${element.image}" alt="imagem produto selecionado">
+                <div class="divInfoProduct">
+                    <h2 class="titleProduct">${element.nome}</h2>
+                    <p class="description">Valor : R$${element.preco}</p>
+                    <button class="btnRemove description">Remover</button>
+                </div>
+                <div class="divQuantidade">
+                    <button class="buttonQuantidade">
+                        <img class="imgIcon" src="assets/chevron-up.svg" alt="Aumentar quantidade">
+                    </button>
+                    <input class="inputQuantidade" type="text" value="1">
+                    <button class="buttonQuantidade">
+                        <img class="imgIcon"  src="assets/chevron-down.svg" alt="Reduzir quantidade">
+                    </button>
+                </div>
+        </li>
+        `
+    })  
+ }
+//A cada vez que o usuário digita um caracter
+//Será preenchido um array com os dados recebidos
 
+const inputSearch = document.querySelector('#inputSearch');
+inputSearch.addEventListener('keyup', (event) => { keyUpping(event) })
+
+function keyUpping(event) {
+    const valorDigitado = event.target.value;
+
+    const arraySearch = searchItems(valorDigitado);
+
+    campoList.innerHTML = '';
+    listarSearch(arraySearch)
+    adicionandoEventButton();
+}
+
+//Utilizando o filter para buscas 
 function searchItems(valorDigitado) {
     return arrayProdutos.filter((element) => {
         return element.nome.toLowerCase().includes(valorDigitado.toLowerCase());
     })
 }
+
--- js/produtos.js
+++ js/produtos.js
@@ -1,60 +1,60 @@
 const arrayProdutos = [
         {
-            "id":1,
-            "nome": "Xiami Redmi Note 8",
+            "id":0,
+            "nome": "Xiaomi Redmi Note 8",
             "preco": "760,00",
             "image": "assets/xiami redmi note 8.jpeg"
         },
         {
-            "id":2,
+            "id":1,
             "nome": "Relógio smartwatch",
             "preco": "496,00",
             "image": "assets/Relógio smartwatch.jpg"
         },
         {
-            "id":3,
+            "id":2,
             "nome": "Smartphone Samsung Galaxy A23 Branco",
             "preco": "1579,00",
             "image": "assets/Smartphone Samsung Galaxy A23 Branco.jpg",
         },
         {
-            "id":4,
+            "id":3,
             "nome": "Computador Pichau Gamer",
             "preco": "2499,99",
             "image": "assets/computador pichau gamer.jpg"
         },
         {
-            "id":5,
+            "id":4,
             "nome": "Headset Gamer RedDragon",
             "preco": "299,99",
             "image": "assets/headset gamer reddragon.jpg"
         },
         {
-            "id":6,
+            "id":5,
             "nome": "Monitor Dell 21.5",
             "preco": "899,99",
             "image": "assets/Monitor 21.5 Dell.jpg"
         },
         {
-            "id":7,
+            "id":6,
             "nome": "Mouse Pad Hyperx",
             "preco": "85,00",
             "image": "assets/Mouse pad hyperx.jpg"
         },
         {
-            "id":8,
+            "id":7,
             "nome": "Mouse RedDragon",
             "preco": "150,00",
             "image": "assets/mouse reddragon.jpg"
         },
         {
-            "id":9,
+            "id":8,
             "nome": "Pc Gamer Itx",
             "preco": "3500,00",
             "image": "assets/pc gamer itx.jpg"
         },
         {
-            "id":10,
+            "id":9,
             "nome": "Teclado gamer hyperx",
             "preco": "250,00",
             "image": "assets/teclado gamer hyperx.jpg"
