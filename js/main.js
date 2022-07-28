dd466778118979f44b2dbc39fadb41ef162656f0
KaioDev08 <kaioaraujo6190@outlook.com>
2022-07-28 06:21:06 -0300

    Adicionando funções de remover e validando erros de adição e remoção de item

--- js/main.js
+++ js/main.js
@@ -36,39 +36,94 @@ function adicionandoEventButton() {
 }
 
 //Adicionando items a bag
-const itemsBag = [];
+let itemsBag = [];
 function addItensBag(id) {
-    itemsBag.push(arrayProdutos[id]);
-    exibirBag(itemsBag);
-    
+    if(validarBag(id) === false || itemsBag.length === 0){
+        itemsBag.push(arrayProdutos[id]);
+        exibirBag(itemsBag);
+        
+        //Adicionando evento de remover item da bag
+        RemoveItem();
+    }
+  
 }
 
- const FieldList = document.querySelector('#listBag');
- function exibirBag(itemsBag) {
-    console.log(itemsBag)
+//Função para verificar se algum item selecionado ja está na lista.
+function validarBag(idclicado){
+    for(let c = 0 ; c < itemsBag.length ; c++)
+       if(itemsBag[c].id == idclicado){
+        return true;
+       }else{
+        if(c === itemsBag.length - 1){
+            return false;
+        }
+       }
+}
+
+const FieldList = document.querySelector('#listBag');
+function exibirBag(itemsBag) {
+    // validarBag(elementClicado);
+   
     FieldList.innerHTML = '';
-    itemsBag.forEach(element =>{
+    itemsBag.forEach(element => {
         FieldList.innerHTML += `
         <li class="itemList itemListBag" id="${element.id}">
             <img class="img" src="${element.image}" alt="imagem produto selecionado">
                 <div class="divInfoProduct">
                     <h2 class="titleProduct">${element.nome}</h2>
                     <p class="description">Valor : R$${element.preco}</p>
-                    <button class="btnRemove description">Remover</button>
+                    <button class="btnRemove description" data-remove>Remover</button>
                 </div>
                 <div class="divQuantidade">
                     <button class="buttonQuantidade">
                         <img class="imgIcon" src="assets/chevron-up.svg" alt="Aumentar quantidade">
                     </button>
-                    <input class="inputQuantidade" type="text" value="1">
+                    <input class="inputQuantidade" data-quantidade id="${element.id}" type="text" value="1">
                     <button class="buttonQuantidade">
                         <img class="imgIcon"  src="assets/chevron-down.svg" alt="Reduzir quantidade">
                     </button>
                 </div>
         </li>
         `
-    })  
- }
+    })
+    
+     //Adicionando evento de remover item da bag
+     RemoveItem();
+}
+
+//Removendo todos os items da bag 
+const buttonRemoveAll = document.querySelector('#buttonRemoveAll');
+buttonRemoveAll.addEventListener('click', RemoveAllItems);
+
+function RemoveAllItems() {
+    FieldList.innerHTML = '';
+    itemsBag = [];
+}
+
+//Removendo item bag 
+
+
+function RemoveItem() {
+    
+    const buttonsRemove = document.querySelectorAll("[data-remove]");
+    buttonsRemove.forEach(element => {
+
+        element.addEventListener('click', (event) => {
+            
+            const itemRemoved  = event.target.parentNode.parentNode.id;
+
+            for(let c=0;c < itemsBag.length;c++) {
+                itemsBag[c].id == itemRemoved ? itemsBag.splice(c, 1) : false ;
+               
+            }
+            
+            exibirBag(itemsBag);
+        });
+    })
+
+}
+
+
 //A cada vez que o usuário digita um caracter
 //Será preenchido um array com os dados recebidos
 
@@ -82,7 +137,10 @@ function keyUpping(event) {
 
     campoList.innerHTML = '';
     listarSearch(arraySearch)
+
+    //Adicionando eventos
     adicionandoEventButton();
+
 }
 
 //Utilizando o filter para buscas 
@@ -92,3 +150,12 @@ function searchItems(valorDigitado) {
     })
 }
 
+//Calculando valor total 
+
+// function verificaQuantidades(){
+//     const inputQuantidade = querySelectorAll('[data-quantidade]')
+//     inputQuantidade.forEach((element) => {
+//         element.addEventListener('click',)
+//     }
+    
+// }
